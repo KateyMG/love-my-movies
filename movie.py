1e336e2a20701474143b27a29102e371c262a475
KateyMG <katherinemazariegos@ufm.edu>
2019-11-06 11:13:30 -0600

    removed a comment

--- movie.py
+++ movie.py
@@ -18,8 +18,7 @@ tmdb.debug= True
 #r.ping
 #r = redis.Redis(host='redis', port=6379, db=0, charset="utf-8", decode_responses=True)
 r=redis.Redis(host="docker_redis",port=6379, db=0)  
-#r = redis.Redis(host='my_redis_service', port=6379, decode_responses=True)
-#redis_server= redis.Redis(host="localhost", port=6379, db=0)
+#r = redis.Redis(host='my_redis_service', port=6379, decode_responses=True)g
 
 
 
